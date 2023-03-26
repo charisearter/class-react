46dadae74cbde5b4f554335c0fb09da8df8f171a
Reese <arter.charise@gmail.com>
2023-03-25 23:27:16 -0700

    initial commit with clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			users: [
+				{
+					id: 1,
+					name: 'A',
+				},
+				{
+					id: 2,
+					name: 'B',
+				},
+				{
+					id: 3,
+					name: 'C',
+				},
+			],
+		};
+	}
+	render() {
+		const { users } = this.state;
+		return (
+			<div>
+				<h1> App component</h1>
+				{users.map((user) => {
+					const { id, name } = user;
+					return <h2 key={id}>{name} </h2>;
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
