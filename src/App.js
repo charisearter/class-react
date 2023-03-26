5cc0daf41d03bc338865453b06aed9dce431a4d1
Reese <arter.charise@gmail.com>
2023-03-25 23:37:01 -0700

    filter works on hardcoded state list

--- src/App.js
+++ src/App.js
@@ -8,25 +8,42 @@ class App extends Component {
 			users: [
 				{
 					id: 1,
-					name: 'A',
+					name: 'Apple',
 				},
 				{
 					id: 2,
-					name: 'B',
+					name: 'Banana',
 				},
 				{
 					id: 3,
-					name: 'C',
+					name: 'Coco',
 				},
 			],
+			searchField: '',
 		};
 	}
+
+	onSearchChange = (e) => {
+		e.preventDefault();
+		const searchField = e.target.value.toLowerCase();
+		this.setState({ searchField });
+	};
+
 	render() {
-		const { users } = this.state;
+		const { users, searchField } = this.state;
+
+		const filteredUsers = users.filter((user) => {
+			return user.name.toLowerCase().includes(searchField);
+		});
 		return (
 			<div>
 				<h1> App component</h1>
-				{users.map((user) => {
+				<input
+					type='text'
+					placeholder='search users'
+					onChange={this.onSearchChange}
+				/>
+				{filteredUsers.map((user) => {
 					const { id, name } = user;
 					return <h2 key={id}>{name} </h2>;
 				})}
